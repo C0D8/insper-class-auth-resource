fc2b727830d9f471c782f6a8e4ba4554b1ff03f4
c0d8 <matheusaguiar362@gmail.com>
2024-04-03 13:30:22 -0300

    aula

---
+++ src/main/java/insper/classroom/auth/Token.java
@@ -0,0 +1,15 @@
+package insper.classroom.auth;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder
+@Accessors(fluent = true, chain = true)
+public record Token (
+
+        String id,
+        String role,
+        String name
+
+) {
+}
