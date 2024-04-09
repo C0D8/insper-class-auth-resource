113e84da5cfecfc8304695dadd55317d4683a038
c0d8 <matheusaguiar362@gmail.com>
2024-04-08 21:34:51 -0300

    swagger

--- src/main/java/insper/classroom/auth/AuthResource.java
+++ src/main/java/insper/classroom/auth/AuthResource.java
@@ -11,14 +11,19 @@ import insper.classroom.auth.LoginOut;
 import insper.classroom.auth.RegisterIn;
 import insper.classroom.auth.SolveIn;
 import insper.classroom.auth.SolveOut;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+
 
 @RestController
+@Tag(name = "Auth Resource", description = "Auth Resource")
 public class AuthResource implements AuthController {
 
     @Autowired
     private AuthService authService;
 
     @Override
+    @Operation(summary = "Create a new account", description = "Create a new account")
     public ResponseEntity<?> create(RegisterIn in) {
 
         final String id = authService.register(Register.builder()
@@ -38,11 +43,13 @@ public class AuthResource implements AuthController {
     }
 
     @Override
+    @Operation(summary = "Authenticate", description = "Authenticate")
     public ResponseEntity<LoginOut> authenticate(CredentialIn in) {
         return ResponseEntity.ok(authService.authenticate(in.email(), in.password()));
     }
 
     @Override
+    @Operation(summary = "Solve", description = "Solve")
     public ResponseEntity<SolveOut> solve(SolveIn in) {
         final Token token = authService.solve(in.token());
         return ResponseEntity.ok(
